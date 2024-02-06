13554757fe606e3b5a0cfdf4fda7684247d4923b
seifmoustafa <seif.moustafa516@gmail.com>
2024-02-06 18:14:33 +0200

    create custom note item

--- lib/main.dart
+++ lib/main.dart
@@ -13,6 +13,7 @@ class NotesApp extends StatelessWidget {
     return MaterialApp(
       theme: ThemeData(
         brightness: Brightness.dark,
+        fontFamily: 'Poppins',
       ),
       home: const NotesView(),
     );
--- lib/widgets/notes_view_body.dart
+++ lib/widgets/notes_view_body.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:note_app/widgets/custom_app_bar.dart';
+import 'package:note_app/widgets/custom_note_item.dart';
 
 class NotesViewBody extends StatelessWidget {
   const NotesViewBody({super.key});
@@ -15,7 +16,8 @@ class NotesViewBody extends StatelessWidget {
           SizedBox(
             height: 50,
           ),
-          CustomAppBar()
+          CustomAppBar(),
+          NoteItem(),
         ],
       ),
     );
