83232220ab7cee93b4b5fa5cc7ce6ee7d78443a0
Seif Moustafa <seif.moustafa516@gmail.com>
2024-02-14 23:42:53 +0200

    add absorb pointer

--- lib/widgets/note_item.dart
+++ lib/widgets/note_item.dart
@@ -8,9 +8,10 @@ import 'package:note_app/views/edit_note_view.dart';
 import 'package:note_app/cubits/notes_cubit/notes_cubit.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:hive/hive.dart';
+// ... (imports and other code)
 
 class NoteItem extends StatefulWidget {
-  const NoteItem({super.key, required this.note});
+  const NoteItem({Key? key, required this.note}) : super(key: key);
   final NoteModel note;
 
   @override
@@ -27,12 +28,12 @@ class _NoteItemState extends State<NoteItem>
     super.initState();
     _controller = AnimationController(
       vsync: this,
-      duration: const Duration(milliseconds: 500),
+      duration: const Duration(seconds: 1),
     );
     _opacityTween = Tween<double>(begin: 1.0, end: 0.0).animate(
       CurvedAnimation(
         parent: _controller,
-        curve: Curves.easeInOut,
+        curve: Curves.fastEaseInToSlowEaseOut,
       ),
     );
   }
@@ -47,66 +48,70 @@ class _NoteItemState extends State<NoteItem>
           );
         }));
       },
-      child: AnimatedBuilder(
-        animation: _controller,
-        builder: (context, child) {
-          return Opacity(
-            opacity: _opacityTween.value,
-            child: Container(
-              padding: const EdgeInsets.only(
-                top: 24,
-                bottom: 24,
-                left: 14,
-              ),
-              decoration: BoxDecoration(
-                color: Color(widget.note.color),
-                borderRadius: BorderRadius.circular(16),
-              ),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.end,
-                children: [
-                  ListTile(
-                    title: Text(
-                      widget.note.title,
-                      style: const TextStyle(color: Colors.black, fontSize: 26),
-                    ),
-                    subtitle: Padding(
-                      padding: const EdgeInsets.only(top: 16, bottom: 16),
-                      child: Text(
-                        widget.note.subTitle.trim(),
-                        style: TextStyle(
-                          color: Colors.black.withOpacity(.5),
-                          fontSize: 18,
+      child: AbsorbPointer(
+        absorbing: _controller.isAnimating,
+        child: AnimatedBuilder(
+          animation: _controller,
+          builder: (context, child) {
+            return Opacity(
+              opacity: _opacityTween.value,
+              child: Container(
+                padding: const EdgeInsets.only(
+                  top: 24,
+                  bottom: 24,
+                  left: 14,
+                ),
+                decoration: BoxDecoration(
+                  color: Color(widget.note.color),
+                  borderRadius: BorderRadius.circular(16),
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  children: [
+                    ListTile(
+                      title: Text(
+                        widget.note.title,
+                        style:
+                            const TextStyle(color: Colors.black, fontSize: 26),
+                      ),
+                      subtitle: Padding(
+                        padding: const EdgeInsets.only(top: 16, bottom: 16),
+                        child: Text(
+                          widget.note.subTitle.trim(),
+                          style: TextStyle(
+                            color: Colors.black.withOpacity(.5),
+                            fontSize: 18,
+                          ),
+                        ),
+                      ),
+                      trailing: IconButton(
+                        onPressed: () {
+                          _controller.forward();
+                          _onDeletePressed();
+                        },
+                        icon: const Icon(
+                          FontAwesomeIcons.trash,
+                          color: Colors.black,
+                          size: 24,
                         ),
                       ),
                     ),
-                    trailing: IconButton(
-                      onPressed: () {
-                        _controller.forward();
-                        _onDeletePressed();
-                      },
-                      icon: const Icon(
-                        FontAwesomeIcons.trash,
-                        color: Colors.black,
-                        size: 24,
+                    Padding(
+                      padding: const EdgeInsets.only(right: 24),
+                      child: Text(
+                        widget.note.date,
+                        style: TextStyle(
+                          color: Colors.black.withOpacity(.4),
+                          fontSize: 16,
+                        ),
                       ),
-                    ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.only(right: 24),
-                    child: Text(
-                      widget.note.date,
-                      style: TextStyle(
-                        color: Colors.black.withOpacity(.4),
-                        fontSize: 16,
-                      ),
-                    ),
-                  )
-                ],
+                    )
+                  ],
+                ),
               ),
-            ),
-          );
-        },
+            );
+          },
+        ),
       ),
     );
   }
@@ -114,7 +119,7 @@ class _NoteItemState extends State<NoteItem>
   void _onDeletePressed() async {
     // Perform any logic you need before or after the animation
     await Future.delayed(
-        const Duration(milliseconds: 500)); // Delay to wait for animation
+        const Duration(seconds: 1)); // Delay to wait for animation
     widget.note.delete();
     await Hive.box<NoteModel>(kNotesBox)
         .delete(widget.note.key); // Adjust box name
